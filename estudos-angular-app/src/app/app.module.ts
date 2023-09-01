9dc4da5a702cb3f84e3f93f4bf8f5328b60496e0
Evan Joseph <evandromariano49@gmail.com>
2023-09-01 09:42:20 -0300

    feat: criado component button

--- estudos-angular-app/src/app/app.module.ts
+++ estudos-angular-app/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu.component';
+import { Button } from './components/button.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent],
+  declarations: [AppComponent, MenuComponent, Button],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- estudos-angular-app/src/app/components/menu.component.ts
+++ estudos-angular-app/src/app/components/menu.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'menu-component',
   template: '<h2>Evan Joseph</h2>',
-  styles: ['h2{ color: blue }'],
+  styles: ['h2 {color: red;}'],
 })
 export class MenuComponent {}
