3cbf652cceeb848fbdc871fe1f16cfaa251ec282
Evan Joseph <evandromariano49@gmail.com>
2023-09-02 14:43:04 -0300

    feat: entendendo two-way data bidding

--- estudos-angular-app/src/app/app.module.ts
+++ estudos-angular-app/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu.component';
 import { Button } from './components/button.component';
 import { Card } from './components/card.component';
+import { EntryDataComponent } from './components/entry-data.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, Button, Card],
+  declarations: [AppComponent, MenuComponent, Button, Card, EntryDataComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
